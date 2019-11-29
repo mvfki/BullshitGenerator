f06602d37eb1a921aa64834f2e0b6f5a478255d7
Yichen Wang <wangych0428@gmail.com>
2019-11-29 03:10:43 -0500

    I hate Chinese Variable Names

--- readJSON.py
+++ readJSON.py
@@ -3,7 +3,7 @@
 # @Date:   2019-11-01 16:52:34
 # @Last Modified by:   Admin
 # @Last Modified time: 2019-11-01 18:18:14
-def 读JSON文件(fileName=""):
+def readJsonFile(fileName=""):
     import json
     if fileName!='':
         strList = fileName.split(".")
